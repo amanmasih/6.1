ac57f8610ccace104c370e3e513dff222f77fd90
AMAN MASIH <39672588+amanmasih@users.noreply.github.com>
2022-02-28 12:42:57 +0530

    Add files via upload

---
+++ productapplication/userproductservice/src/main/java/com/niit/userproductservice/UserproductserviceApplication.java
@@ -0,0 +1,13 @@
+package com.niit.userproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserproductserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserproductserviceApplication.class, args);
+	}
+
+}
